20292e2c270add658b61359b1c7e35d56e3fec1d
kangm7890 <78109886+kangm7890@users.noreply.github.com>
2021-01-28 13:17:08 -0800

    Unit 00 first push

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,29 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Matthew Kang \n\n" );
+		System.out.println("fish" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("           _____   _            " );
+		System.out.println("          /.    \\/ |         " );
+		System.out.println("         |      |  |       " );
+		System.out.println("          \\_____/  |              ");
+		System.out.println("                 \\_|               ");
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,21 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Matthew Kang \t  January 28th \n\n" );
+		System.out.println(" _________________________" );
+		for (int i = 0; i < 7; i++) {
+			System.out.println("|                         |");
+		}
+		System.out.println("|_________________________|");
+
+	}
+}
